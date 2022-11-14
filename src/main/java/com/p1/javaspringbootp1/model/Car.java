9068fec04385caf79a0fb347e4a5357298cd4f30
NelsonJ <jesus.nels@gmail.com>
2022-11-14 14:42:06 +0100

    Car class defined

--- src/main/java/com/p1/javaspringbootp1/model/Car.java
+++ src/main/java/com/p1/javaspringbootp1/model/Car.java
@@ -1,21 +1,74 @@
-package com.p1.javaspringbootp1.model;/**
+package com.p1.javaspringbootp1.model;
 
+/*
  * @author nelsj
+ * @Date 14/11/2022
+ */
 
- * @Date 14/11/2022 
+public class Car {
 
- */public class Car {
-
-// -------------- ATTRIBUTE(s) -------------- //
-
-
-// ------------- CONSTRUCTOR(s) ------------- //
-
-
-// --------- GETTER(s) & SETTER(s) ---------- //
+    // -------------- ATTRIBUTE(s) -------------- //
+    private int id;
+    private String model;
+    private String make;
+    private String color;
 
 
-// --------------- METHODS(s) --------------- //
+    // ------------- CONSTRUCTOR(s) ------------- //
+    public Car() {
 
+    }
+
+    public Car(int id, String model, String make, String color) {
+        this.id = id;
+        this.model = model;
+        this.make = make;
+        this.color = color;
+    }
+
+    // --------- GETTER(s) & SETTER(s) ---------- //
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.model = make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    // --------------- METHODS(s) --------------- //
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model=" + model +
+                ", make=" + make +
+                ", color=" + color +
+                '}';
+    }
 
 }
