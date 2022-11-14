3370b67ebab64eac155ed5393dc398efa08622b9
NelsonJ <jesus.nels@gmail.com>
2022-11-14 14:40:30 +0100

    Hello world commit

--- src/test/java/com/p1/javaspringbootp1/JavaSpringBootP1ApplicationTests.java
+++ src/test/java/com/p1/javaspringbootp1/JavaSpringBootP1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaSpringBootP1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
