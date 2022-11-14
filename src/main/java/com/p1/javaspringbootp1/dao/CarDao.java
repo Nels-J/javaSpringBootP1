a3ba0f1f380c864dc569217c6b48a4c1a3132ba0
NelsonJ <jesus.nels@gmail.com>
2022-11-14 14:44:03 +0100

    DAO interface added and implemented on CarDaoImpl

---
+++ src/main/java/com/p1/javaspringbootp1/dao/CarDao.java
@@ -0,0 +1,18 @@
+package com.p1.javaspringbootp1.dao;
+
+import com.p1.javaspringbootp1.model.Car;
+
+import java.util.List;
+
+/**
+ * @author nelsj
+ * @Date 14/11/2022
+ */
+public interface CarDao {
+    List<Car> findAll();
+
+    Car findById(int id);
+
+    Car save(Car car);
+
+}
---
+++ src/main/java/com/p1/javaspringbootp1/dao/CarDaoImpl.java
@@ -0,0 +1,37 @@
+package com.p1.javaspringbootp1.dao;
+
+import com.p1.javaspringbootp1.model.Car;
+
+import java.util.List;
+
+/**
+ * @author nelsj
+ * @Date 14/11/2022
+ */
+
+public class CarDaoImpl implements CarDao {
+// -------------- ATTRIBUTE(s) -------------- //
+
+
+// ------------- CONSTRUCTOR(s) ------------- //
+
+
+// --------- GETTER(s) & SETTER(s) ---------- //
+
+
+    // --------------- METHODS(s) --------------- //
+    @Override
+    public List<Car> findAll() {
+        return null;
+    }
+
+    @Override
+    public Car findById(int id) {
+        return null;
+    }
+
+    @Override
+    public Car save(Car car) {
+        return null;
+    }
+}
